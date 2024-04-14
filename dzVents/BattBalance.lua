5341e4369b14c5ac9053554348cfff448020f72d
Rob Peters <info@domoticz.com>
2024-04-14 14:17:57 +0200

    Updated balance scripts

--- dzVents/BattBalance.lua
+++ dzVents/BattBalance.lua
@@ -1,46 +1,69 @@
-disabled at the moment
---
--- set the ESS setpoint according to the energy needs of the house
--- when battery is in Balance mode
--- and more than 250W difference
---
 return {
-	active = true,
-	logging = { level = domoticz.LOG_NORMAL, marker = "BATTBALANCE" },
 	on = {
-		devices = { 'Battery Mode', 'Elektra Meter' },
-		--timer = { 'Every minute' } 
+		timer = {
+			'every minute'
+		},
+	    devices = {
+			'Battery Mode'
+		}		
 	},
-	execute = function(dz, dev)
-
-		local bms = dz.devices('Battery Mode').state
+	logging = {
+		level = domoticz.LOG_INFO,
+		marker = 'batt_balance',
+	},
+	execute = function(dz, timer)
+		local batt_mode = dz.devices('Battery Mode')
 		local car_charging = dz.devices('Laadpaal Charging').active
 
+        if (batt_mode.state == 'Grid Balance') and (car_charging == false) then
 
-		if ((bms == 'Balance') and (car_charging == false)) then
-			power=dz.devices('Elektra Meter').usage - dz.devices('Elektra Meter').usageDelivered
-			if (math.abs(power) > 250) then 
-				-- check if we need to adjust power
-				--print('Power is '..power)
-				local bm = dz.devices('Battery Mode')
-				local ess_sp = dz.devices('ESS Setpoint')
-				local sp = dz.devices('ESS Setpoint').setPoint
-				local bat_sp = dz.devices('ESS Setpoint')
-				local batt_soc = dz.devices('Battery SOC').percentage
+    		local batt_soc = dz.devices('Battery SOC').percentage
+            local min_soc_level = dz.variables('min_soc_level').value
+            local max_soc_level = dz.variables('max_soc_level').value
+    
+    	    local batt_sp = dz.devices('ESS Setpoint')
+    	    local batt_sp_value = batt_sp.setPoint
+    	    local idle_rate = dz.variables('IdleRate').value
+    		local charge_rate = dz.variables('ChargeRate').value
+    		local discharge_rate = dz.variables('DischargeRate').value
 
-				if power > 0 then
-					new_sp = -power
-					print('ESS Setpoint needs adjusting, power: '..power..' from: '..sp..' to '..new_sp)
-					ess_sp.updateSetPoint(new_sp)
-					if (batt_soc <= 10) then -- stop compensating if we are approaching the last 10% SOC
-						bm.switchSelector('Idle')
-					end
-				else
-					new_sp = -power
-					print('ESS Setpoint needs adjusting, power: '..power..' from: '..sp..' to '..new_sp)
-					ess_sp.updateSetPoint(new_sp)
-				end
-			end
-		end
+        	local new_setpoint = batt_sp_value
+    		
+    		local current_usage = dz.devices('Grid L3 Watt').actualWatt
+    		
+    		if (current_usage > 5000) then
+        		print('>>> Grid Usage to High! (' .. current_usage .. ' Watt), switching to IDLE value')
+        		new_setpoint = idle_rate
+    		else
+        	    grid_power = dz.devices('Elektra Meter').usage - dz.devices('Elektra Meter').usageDelivered
+            	print('grid value: ' .. grid_power)
+
+        	    new_setpoint = batt_sp_value - (grid_power/2)
+            end
+        
+            --we always make sure to have atleast the idle rate value
+            if (new_setpoint < idle_rate) then
+                new_setpoint = idle_rate
+            elseif (new_setpoint < discharge_rate) then
+                new_setpoint = discharge_rate
+            elseif (new_setpoint > charge_rate) then
+                new_setpoint = charge_rate
+            end
+            
+            if ((new_setpoint < 0) and (batt_soc >=max_soc_level)) or ((new_setpoint > 0) and (batt_soc <= min_soc_level)) then
+                --Also handled in batt_control script
+                 new_setpoint = idle_rate
+            end
+
+        	if (new_setpoint ~= batt_sp_value) then
+                diff_sp = math.abs(new_setpoint - batt_sp_value)
+                if (diff_sp > 50) or ( new_setpoint == idle_rate) then
+                	print('old_setpoint: ' .. batt_sp_value)
+                	print('new_setpoint: ' .. new_setpoint)
+                    batt_sp.cancelQueuedCommands()
+        		    batt_sp.updateSetPoint(new_setpoint)
+        		end
+        	end
+    	end
 	end
 }
--- dzVents/BattSolarCharge.lua
+++ dzVents/BattSolarCharge.lua
@@ -3,9 +3,9 @@ return {
 		timer = {
 			'every minute'
 		},
-	    --devices = {
-		--	'Elektra Meter'
-		--}		
+	    devices = {
+			'Battery Mode'
+		}		
 	},
 	logging = {
 		level = domoticz.LOG_INFO,
@@ -14,39 +14,62 @@ return {
 	execute = function(dz, timer)
 		local batt_mode = dz.devices('Battery Mode')
 		local car_charging = dz.devices('Laadpaal Charging').active
-		local batt_soc = dz.devices('Battery SOC').percentage
-        local max_soc_level = dz.variables('max_soc_level').value
 
         if (batt_mode.state == 'Solar Charge') and (car_charging == false) then
+    
+    		local batt_soc = dz.devices('Battery SOC').percentage
+            local max_soc_level = dz.variables('max_soc_level').value
+    
             if (batt_soc < max_soc_level) then
         	    local batt_sp = dz.devices('ESS Setpoint')
         	    local batt_sp_value = batt_sp.setPoint
         	    local idle_rate = dz.variables('IdleRate').value
-                
-        	    grid_power = dz.devices('Elektra Meter').usage - dz.devices('Elektra Meter').usageDelivered
+        		local charge_rate = dz.variables('ChargeRate').value
 
-            	if (grid_power > 0) then
-            	    grid_power = 0
-            	end
-            	
-            	local new_setpoint = batt_sp_value + ((-grid_power - batt_sp_value) / 2)
+            	local new_setpoint = batt_sp_value
+
+                local current_usage = dz.devices('Grid L3 Watt').actualWatt
+                
+        		if (current_usage > 5000) then
+            		print('>>> Grid Usage to High! (' .. current_usage .. ' Watt), switching to IDLE value')
+            		new_setpoint = idle_rate
+        		else
+            	    grid_power = dz.devices('Elektra Meter').usage - dz.devices('Elektra Meter').usageDelivered
+                	print('grid value: ' ..grid_power)
+                	if (grid_power > 0) then
+                	    grid_power = 0
+                	end
+    
+            	    new_setpoint = batt_sp_value - (grid_power/2)
         
-                --we always make sure to have atleast the idle rate value
-                if (new_setpoint < idle_rate) then
-                    new_setpoint = idle_rate
+                    --we always make sure to have atleast the idle rate value
+                    if (new_setpoint < idle_rate) then
+                        new_setpoint = idle_rate
+                    elseif (new_setpoint > charge_rate) then
+                        new_setpoint = charge_rate
+                    end
                 end
             	
-            	print('Grid value: ' ..grid_power)
-            	print('new_setpoint: ' ..new_setpoint)
+                if ((new_setpoint < 0) and (batt_soc >=max_soc_level)) or ((new_setpoint > 0) and (batt_soc <= min_soc_level)) then
+                    --Also handled in batt_control script
+                     new_setpoint = idle_rate
+                end
             	
             	if (new_setpoint ~= batt_sp_value) then
-                    batt_sp.cancelQueuedCommands()
-        		    batt_sp.updateSetPoint(new_setpoint)
+                    diff_sp = math.abs(new_setpoint - batt_sp_value)
+                    if (diff_sp > 50) or ( new_setpoint == idle_rate) then
+                    	print('old_setpoint: ' ..batt_sp_value)
+                    	print('new_setpoint: ' ..new_setpoint)
+                        batt_sp.cancelQueuedCommands()
+            		    batt_sp.updateSetPoint(new_setpoint)
+            		end
         		end
             else
                 --we are ready
-                batt_sp.cancelQueuedCommands()
-                batt_sp.updateSetPoint(idle_rate.value)
+                if (batt_sp_value ~= idle_rate.value) then
+                    batt_sp.cancelQueuedCommands()
+                    batt_sp.updateSetPoint(idle_rate.value)
+                end
     	    end
     	end
 	end
