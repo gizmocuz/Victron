186a834bd2a23ebae6990479b79107b5734c2633
Rob Peters <info@domoticz.com>
2024-05-01 09:03:39 +0200

    Making sure we won't get stuck on discharge_rate

--- dzVents/BattSolarAndBalance.lua
+++ dzVents/BattSolarAndBalance.lua
@@ -15,6 +15,7 @@ return {
 	data =
     {
         GridPowerHistory = { history = true, maxMinutes = 5 },
+	    last_max_adjusted = { initial = 0 },
     },
 	execute = function(dz, dev)
 	    if (dev.name == 'Elektra Meter') then
@@ -54,10 +55,14 @@ return {
             		new_setpoint = idle_rate
         		else
             	    grid_power = dz.data.GridPowerHistory.avg()
+            	    --grid_power_med = dz.data.GridPowerHistory.med()
+            	    --dz.log("avg_grid_pwr: " .. grid_power .. ", med: " .. grid_power_med)
+            	    --grid_power = grid_power_med
 
             	    --new_setpoint = batt_sp_value - (grid_power/2) --not sure if still need to do this as we are already averaging!
             	    new_setpoint = math.floor(batt_sp_value - grid_power + 0.5)
-            	    
+            	    --dz.log("new_setpoint: " .. new_setpoint)
+
             	    if (bIsSolarChargeMode) then
                         if (new_setpoint < idle_rate) then
                             new_setpoint = idle_rate
@@ -65,6 +70,21 @@ return {
             	    else
                         if (new_setpoint < discharge_rate) then
                             new_setpoint = discharge_rate
+                            if (batt_sp_value <= discharge_rate) then
+                                local current_time = os.time()
+                                if (dz.data.last_max_adjusted ~= 0) then
+                                    local timediff = current_time - dz.data.last_max_adjusted
+                                    --dz.log("timediff: " .. timediff)
+                                    if (timediff >= 900) then
+                                        -- it could be we are stuck on the bottom... lift us up
+                                        -- otherwise we have a very high load for half an 15 minutes!
+                                        new_setpoint = math.floor(0 - grid_power + 0.5)
+                                        dz.data.last_max_adjusted = 0
+                                    end
+                                else
+                                    dz.data.last_max_adjusted = os.time()
+                                end
+                            end
                         end
             	    end
                     if (new_setpoint > charge_rate) then
